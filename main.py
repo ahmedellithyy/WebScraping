c0f482e90d868e14899c9f586f7fffff790ac7a8
ahmeedellithy <ellithy12344321@gmail.com>
2021-04-01 19:23:49 +0200

    first trial

--- main.py
+++ main.py
@@ -13,4 +13,8 @@ src = result.content
 soup = BeautifulSoup(src, "lxml")
 print(soup)
 
-job_titles = soup.find_all("a", {"class":"css-m604qf"})
+job_titles = soup.find_all("h2", {"class": "css-m604qf"})
+company_names = soup.find_all("a",{"class": "css-17s97q8"})
+locations = soup.find_all("span", {"class":"css-5wys0k"})
+jobs_skills = soup.find_all("div",{"class":"css-y4udm8"})
+print(job_titles)
