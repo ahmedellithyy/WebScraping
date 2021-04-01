2302033fe39097574f62f3e0e8bca050e6a2f3ab
ahmeedellithy <ellithy12344321@gmail.com>
2021-04-01 19:38:05 +0200

    second update

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import csv
 from itertools import zip_longest
 
+
+job_title = []
+company_name = []
+location = []
+job_skills = []
+
+
 # Use requests to fetch URL
 result = requests.get("https://wuzzuf.net/search/jobs/?q=asp.net&a=hpb")
 
@@ -17,4 +24,18 @@ job_titles = soup.find_all("h2", {"class": "css-m604qf"})
 company_names = soup.find_all("a",{"class": "css-17s97q8"})
 locations = soup.find_all("span", {"class":"css-5wys0k"})
 jobs_skills = soup.find_all("div",{"class":"css-y4udm8"})
-print(job_titles)
+
+
+for i in range(len(job_titles)):
+    job_title.append(job_titles[i].text)
+
+for i in range(len(company_names)):
+    company_name.append(company_names[i].text)
+
+for i in range(len(locations)):
+    location.append(locations[i].text)
+
+for i in range(len(jobs_skills)):
+    job_skills.append(jobs_skills[i].text)
+
+with open("")
