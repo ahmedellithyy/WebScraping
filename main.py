bfb2ae804be2c34d3a48a2b66678a51f2c2b74f5
ahmeedellithy <ellithy12344321@gmail.com>
2021-04-01 19:05:47 +0200

    first trial

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ from bs4 import BeautifulSoup
 import csv
 from itertools import zip_longest
 
+# Use requests to fetch URL
+result = requests.get("https://wuzzuf.net/search/jobs/?q=asp.net&a=hpb")
 
+# Save the content of result into src
+src = result.content
+
+# Create a soup object to parse content of the page
+soup = BeautifulSoup(src, "lxml")
+print(soup)
+
+job_titles = soup.find_all("a", {"class":"css-m604qf"})
